27cb4fe8b96ee277caa0995df5e267c71e651e11
bqugihan <bqugihan@bqugihan-PC.bquzone.lk>
2015-08-06 13:02:23 +0530

    xasasd

---
+++ example-page2.php
@@ -0,0 +1,25 @@
+<div class="col-md-6">
+				<div class="content_box"><a href="single.html">
+			   	  <div class="view view-fifth" >
+			   	   	 <img src="images//pic8.jpg" class="img-responsive " style="min-width:100% ; max-height:280px;"  alt=""/>
+				   	   <div class="mask1">
+	                        <div class="info"> </div>
+			            </div>
+				   	  </a>
+				   </div>
+				     <h5><a href="details.html"> Duis autem</a></h5>
+				    <h6>It is a long established fact that a reader<h6>
+				     Rs. 499
+			   	</div>
+			</div>
+			
+		<?php
+		$id=0;
+		if(isset($_GET['id'])){
+		$id=$_GET['id']+1;
+		
+		}
+		
+		
+		 ?>	
+			
